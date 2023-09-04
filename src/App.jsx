bf43c094aca894f8589d823b253128a4fc85c8cf
PruthviMogaveer <pruthvimogaveer18@gmail.com>
2023-09-04 14:00:53 +0530

    starting of fusion shoes landing page

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import {
+  CustomerReviews,
+  Footer,
+  PopularProduct,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+  Hero
+} from "./sections"
+import Nav from "./components/nav"
+
+const App = () => {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProduct />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Hero from "./Hero"
+
+export {
+  CustomerReviews,
+  Footer,
+  PopularProduct,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+  Hero
+};
