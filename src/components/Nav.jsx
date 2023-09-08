5e242331b7c1b0ea896c9bf78fec4756adb4fa7d
PruthviMogaveer <pruthvimogaveer18@gmail.com>
2023-09-08 10:58:38 +0530

    new commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray hover:text-black"
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:text-black active:text-black"
               >
                 {item.label}
               </a>
