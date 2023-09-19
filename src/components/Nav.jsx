d7ec39520b5f73654bae9da26e3fc23dd94db217
PruthviMogaveer <pruthvimogaveer18@gmail.com>
2023-09-19 09:58:19 +0530

    added menu for mobile devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,10 @@
 import { headerLogo } from "../../assets/images";
 import { hamburger } from "../../assets/icons";
 import { navLinks } from "../constants";
+import { useState } from "react";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header className="padding-x py-8 mx-[-10px] absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
@@ -15,9 +17,20 @@ const Nav = () => {
             className="m-0 w-40 h-20"
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul
+          className={`flex-1 flex justify-center items-center gap-16 ${
+            menuOpen ? `max-lg:max-lg-menu` : `max-lg:hidden`
+          }`}
+        >
+          {menuOpen && window.innerWidth < 1024 ? (
+            <div onClick={() => setMenuOpen(!menuOpen)}>
+              <i class="fa-solid fa-xmark fa-2xl text-slate-gray"></i>
+            </div>
+          ) : (
+            <div></div>
+          )}
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <li key={item.label} onClick={() => setMenuOpen(!menuOpen)}>
               <a
                 href={item.href}
                 className="font-montserrat leading-normal text-lg text-slate-gray hover:text-black active:text-black"
@@ -28,7 +41,10 @@ const Nav = () => {
           ))}
         </ul>
 
-        <div className="hidden max-lg:block">
+        <div
+          className="hidden max-lg:block"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
           <img src={hamburger} alt="hamburger icon" width={25} height={25} />
         </div>
       </nav>
